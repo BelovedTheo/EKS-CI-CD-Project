edb8f0b5baaf8b95ecba49898328d26b6a90fe46
BelovedTheo <vziulin@gmail.com>
2024-09-11 00:54:01 +0300

    latest

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -282,7 +282,7 @@ resource "aws_eks_node_group" "public" {
 
   scaling_config {
     desired_size = 0
-    max_size     = 0
+    max_size     = 1
     min_size     = 0
   }
 
@@ -313,7 +313,7 @@ resource "aws_eks_node_group" "private" {
 
   scaling_config {
     desired_size = 0
-    max_size     = 0
+    max_size     = 1
     min_size     = 0
   }
 
